6c15ca00cb409f92090b2aeac8e7925c75f62ab9
David <david.djdim@gmail.com>
2019-01-25 17:15:59 -0600

    Reusando restserver

--- server/server.js
+++ server/server.js
@@ -13,22 +13,31 @@ app.use(bodyParser.json())
 
 app.get('/usuario', function (req, res) {
   //res.send('Hello World')
-  res.json('get Usuario')
+  res.json('Usando get con Usuario Local')
 })
 
 app.post('/usuario', function (req, res) {
-  let persona = req.body;
+  let body = req.body;
+  
+  if (body.nombre === undefined){
+    res.status(400).json({
+      ok:false,
+      mensaje: 'El nombre es necesario'
+    });
+  }
+  else{
   res.json({
-  	persona:persona
-  })
-})
+  	persona :body
+  });
+  }
+});
+
 
 app.put('/usuario/:id', function (req, res) {
   //res.send('Hello World')
   let id= req.params.id;
   res.json({
-  		id:id,
-
+  		id
 			});
 });
 
